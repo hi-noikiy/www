ed74baee546a8f33d95d69dc1824f64e9537923a
김석태 <bori0211@gmail.com>
2018-01-17 16:36:25 +0900

    vue vuex sample 1

---
+++ vue-vuex-sample-1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h1>Vuex</h1>
+          <app-result></app-result>
+          <app-another-result></app-another-result>
+          <hr>
+          <app-counter></app-counter>
+          <app-another-counter></app-another-counter>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+import AnotherCounter from "./components/AnotherCounter.vue";
+import Result from "./components/Result.vue";
+import AnotherResult from "./components/AnotherResult.vue";
+
+export default {
+  name: 'app',
+  components: {
+    appCounter: Counter,
+    appAnotherCounter: AnotherCounter,
+    appResult: Result,
+    appAnotherResult: AnotherResult
+  }
+}
+</script>
---
+++ vue-vuex-sample-1/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="increment">Increment</button>
+    <button class="btn btn-primary" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+  /*
+  methods: {
+    increment() {
+      //this.$store.state.counter++;
+      this.$store.commit("increment");
+    },
+    decrement() {
+      //this.$store.state.counter--;
+      this.$store.commit("decrement");
+    }
+  }
+  */
+  methods: {
+    ...mapMutations([
+      "increment",
+      "decrement"
+    ])
+  }
+}
+</script>
