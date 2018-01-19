3382297b6487c002bade5dee62f8e2be74de2a0d
김석태 <bori0211@gmail.com>
2018-01-19 12:55:52 +0900

    vue-project-3

---
+++ vue-project-3/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Trade of View your Portfolio</h1>
+    <h6>You may Save & Load your Data</h6>
+    <hr>
+    <p>Your Funds: {{ funds | currency }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
+}
+</script>
